e70305ad005ef1468b022022b1f68c561abc965c
Thales Moreira <thales.moreira@hotmail.com>
2021-06-29 21:22:30 -0300

    Criando o Primeiro Cenário

--- src/test/java/br/ufrn/sigaa/AreaPublica.java
+++ src/test/java/br/ufrn/sigaa/AreaPublica.java
@@ -6,8 +6,14 @@ import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Quando;
 import io.cucumber.java.pt.Entao;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.concurrent.TimeUnit;
 
 public class AreaPublica {
     public String url;
@@ -30,23 +36,32 @@ public class AreaPublica {
         driver.get(url);
     }
     @Quando("usuario seleciona a opcao componentes curriculares no menu graduacao")
-    public void usuario_seleciona_a_opcao_componentes_curriculares_no_menu_graduacao() {
-        // Write code here that turns the phrase above into concrete actions
+    public void usuario_seleciona_a_opcao_componentes_curriculares_no_menu_graduacao() throws InterruptedException {
+        driver.findElement(By.id("l-graduacao")).click();
+        TimeUnit.SECONDS.sleep(1);
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div[2]/div[2]/div[2]/div/div[2]/dl/dt[2]/div/a")).click();
 
     }
     @Entao("o sistema deve exibir o form de busca por Componentes")
     public void o_sistema_deve_exibir_o_form_de_busca_por_componentes() {
         // Write code here that turns the phrase above into concrete actions
-
+        String expected;
+        expected = "Consulta de Componentes Curriculares";
+        String actual = driver.findElement(By.cssSelector("#corpo > h2")).getText();
+        Assert.assertEquals(expected, actual);
     }
-    @Quando("o cliente busca pelo componete de {string} {string}")
-    public void o_cliente_busca_pelo_componete_de(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
+    @Quando("o usuario busca pelo componete de GRADUACAO {string}")
+    public void o_usuario_busca_pelo_componete_de_graduacao(String string) {
+        WebElement nivel = driver.findElement(By.id("form:nivel"));
+        new Select(nivel).selectByVisibleText(("GRADUAÇÃO"));
+        driver.findElement(By.name("form:j_id_jsp_190531263_11")).sendKeys(string);
+        driver.findElement(By.id("form:btnBuscarComponentes")).click();
 
     }
     @Entao("o sistema deve exibir o componente {string} na lista de resultados")
     public void o_sistema_deve_exibir_o_componente_na_lista_de_resultados(String string) {
-        // Write code here that turns the phrase above into concrete actions
-
+        String expected = "FUNDAMENTOS MATEMÁTICOS DA COMPUTAÇÃO I";
+        String actual = driver.findElement(By.cssSelector("#formListagemComponentes > table > tbody > tr > td:nth-child(2)")).getText();
+        Assert.assertEquals(expected, actual);
     }
 }
--- target/test-classes/AreaPublica.feature
+++ target/test-classes/AreaPublica.feature
@@ -4,5 +4,5 @@
       Dado que o usuario acessa a pagina publica do SIGAA
       Quando usuario seleciona a opcao componentes curriculares no menu graduacao
       Entao o sistema deve exibir o form de busca por Componentes
-      Quando o cliente busca pelo componete de "GRADUAÇÂO" "IMD0028"
+      Quando o usuario busca pelo componete de GRADUACAO "IMD0028"
       Entao o sistema deve exibir o componente "Fundamentos Matemáticos da Computacão" na lista de resultados
